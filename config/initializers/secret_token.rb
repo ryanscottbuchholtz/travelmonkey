5c8c3763b2311a5b3f5ab72f71170588050f79f0
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2014-01-27 10:30:45 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Travelmonkey::Application.config.secret_key_base = 'aa4179fea3581e6033e128e1d8956df7d2a20608e3e984c4a10e16b452ff44c01eac1c9b299cd40a176011441d39c286fe40662cc12f2b9e66e760855c0104a8'
