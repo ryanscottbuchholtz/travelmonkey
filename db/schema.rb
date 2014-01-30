f2a305d3c491aa22f48a072ddb6b513a5889d9b4
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2014-01-30 11:13:51 -0500

    added user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140127155038) do
+ActiveRecord::Schema.define(version: 20140127165811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,5 +27,34 @@ ActiveRecord::Schema.define(version: 20140127155038) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "first_name",                          null: false
+    t.string   "middle_name"
+    t.string   "last_name",                           null: false
+    t.string   "birth_month",                         null: false
+    t.integer  "birth_day",                           null: false
+    t.integer  "birth_year",                          null: false
+    t.string   "gender"
+    t.string   "home_city"
+    t.string   "home_state"
+    t.string   "home_country"
+    t.string   "home_zip"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
